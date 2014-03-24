b2df570a2240e492c02a7648d43101287a249001
m-garanin <m-garanin@yandex.ru>
2014-03-24 12:31:30 +0400

    parametrized

--- hcp/sql/devdb.sh
+++ hcp/sql/devdb.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-psql -d template1 -c "DROP DATABASE test_hcp;"
-psql -d template1 -c "CREATE DATABASE test_hcp ENCODING='UNICODE';"
-psql -d test_hcp -f schema.sql
-cat test_data.csv | psql -d test_hcp -c "COPY npidatas FROM STDIN WITH (FORMAT CSV, HEADER TRUE);ALTER TABLE npidatas ADD COLUMN id SERIAL PRIMARY KEY;"
+psql -d template1 -c "DROP DATABASE test_hcp;" -U $1
+psql -d template1 -c "CREATE DATABASE test_hcp ENCODING='UNICODE';" -U $1
+psql -d test_hcp -f schema.sql -U $1
+cat test_data.csv | psql -d test_hcp -c "COPY npidatas FROM STDIN CSV HEADER;ALTER TABLE npidatas ADD COLUMN id SERIAL PRIMARY KEY;" -U $1
+psql -d test_hcp -f indx.sql -U $1
+
